8b3b30c58819b054bfe701d0c2d7d12f7e05363d
bedboy92 <120000151+bedboy92@users.noreply.github.com>
2022-12-29 17:56:17 +0900

    Update add_ssh_auth.sh

--- add_ssh_auth.sh
+++ add_ssh_auth.sh
@@ -2,3 +2,5 @@
 
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node01
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node02 
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node03
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node04 
